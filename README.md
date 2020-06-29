9b36018f9df11f91da1b1ea2284da4d4ce1afc5b
ninnart-fuengfusin <ninnart.fuengfusin@yahoo.com>
2020-06-29 14:29:34 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# ninutils
-Wrapper of Python with only standard libraries.
+# nincore
+A wrapper of Python using only standard libraries. <br>
+This module is designed to be my base requirement of my further modules. <br>
+
+## To install
+```
+pip install nincore
+```
+
+## License
+MIT License with some exceptions. <br>
+Please look at comments within the module.
+
+
+## TODO
+- All.
---
+++ __init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from .nincore import *
+from . import check
+from . import typing
+from . import path
+from . import utils
+from . import recorder
+from . import error
+
+
+__all__ = []
+__all__ += check.__all__
+__all__ += typing.__all__
+__all__ += path.__all__
+__all__ += utils.__all__
+__all__ += recorder.__all__
+__all__ += error.__all__
